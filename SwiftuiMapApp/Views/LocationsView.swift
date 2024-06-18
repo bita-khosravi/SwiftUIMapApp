c4b12c5bc27652c4a809fcd1d8cd5fd5b657eff9
Bita <bita.khosraavii@gmail.com>
2024-06-18 21:36:31 +0200

    Cleaning project structure

--- SwiftuiMapApp/Views/LocationsView.swift
+++ SwiftuiMapApp/Views/LocationsView.swift
@@ -15,44 +15,14 @@ struct LocationsView: View {
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $vm.mapRegion)
-            Map(
-                coordinateRegion: $vm.mapRegion,
-                annotationItems: vm.locations,
-                annotationContent: { location in
-                    MapAnnotation(coordinate: location.coordinates) {
-                        LocationMapAnnotationView()
-                            .scaleEffect(vm.mapLocation == location ? 1.1 : 0.7)
-                            .shadow(radius: 10)
-                            .onTapGesture {
-                                vm.showNextLocation(location: location)
-                            }
-                    }
-                }
-            )
+            mapLayer
                 .ignoresSafeArea()
             
             VStack(spacing: 0) {
                 header
                     .padding()
-                
                 Spacer()
-                
-                ZStack {
-                    ForEach(vm.locations) { location in
-                        if vm.mapLocation == location {
-                            LocationPreviewView(location: vm.mapLocation)
-                                .shadow(color: Color.black.opacity(0.3), radius: 20)
-                                .padding()
-                                .transition(
-                                    .asymmetric(
-                                        insertion: .move(edge: .trailing),
-                                        removal: .move(edge: .leading)
-                                    )
-                                )
-                        }
-                    }
-                }
+                loccationsPreviewStack
             }
             
             
@@ -102,6 +72,41 @@ extension LocationsView {
         .shadow(color: Color.black.opacity(0.4), radius: 20, x: 0, y: 15)
         
         
+    }
+    
+    private var mapLayer: some View {
+        Map(
+            coordinateRegion: $vm.mapRegion,
+            annotationItems: vm.locations,
+            annotationContent: { location in
+                MapAnnotation(coordinate: location.coordinates) {
+                    LocationMapAnnotationView()
+                        .scaleEffect(vm.mapLocation == location ? 1.1 : 0.7)
+                        .shadow(radius: 10)
+                        .onTapGesture {
+                            vm.showNextLocation(location: location)
+                        }
+                }
+            }
+        )
+    }
+    
+    private var loccationsPreviewStack: some View {
+        ZStack {
+            ForEach(vm.locations) { location in
+                if vm.mapLocation == location {
+                    LocationPreviewView(location: vm.mapLocation)
+                        .shadow(color: Color.black.opacity(0.3), radius: 20)
+                        .padding()
+                        .transition(
+                            .asymmetric(
+                                insertion: .move(edge: .trailing),
+                                removal: .move(edge: .leading)
+                            )
+                        )
+                }
+            }
+        }
     }
 }
 
